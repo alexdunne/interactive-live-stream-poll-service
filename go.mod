7c9c50bcecc16a0a8d9b982b1ed389fe9564f004
Alex Dunne <hi@alexdunne.net>
2022-07-02 23:33:54 +0100

    implement storing polls

--- go.mod
+++ go.mod
@@ -6,14 +6,17 @@ replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 
 require (
 	github.com/aws/aws-lambda-go v1.23.0
+	github.com/aws/aws-sdk-go v1.44.47
 	github.com/go-playground/locales v0.14.0
 	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-playground/validator/v10 v10.11.0
+	github.com/google/uuid v1.3.0
 )
 
 require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- handlers/create-poll/main.go
+++ handlers/create-poll/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/alexdunne/interactive-live-stream-poll-service/internal/api"
 	"github.com/alexdunne/interactive-live-stream-poll-service/internal/validator"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/google/uuid"
 )
+
+const _tableNameEnv = "POLL_TABLE_NAME"
 
 type createPollRequest struct {
 	Question   string   `json:"question" validate:"required,min=1,max=100"`
@@ -17,16 +26,43 @@ type createPollRequest struct {
 	ChannelARN string   `json:"channelARN" validate:"required,min=20,max=2048"`
 }
 
+type createPollResponse struct {
+	ID string `json:"id"`
+}
+
+type poll struct {
+	PK                   string         `json:"PK"`
+	SK                   string         `json:"SK"`
+	ID                   string         `json:"id"`
+	Question             string         `json:"question"`
+	Options              []pollOption   `json:"options"`
+	ChannelARN           string         `json:"channelARN"`
+	AggregatedVoteTotals map[string]int `json:"aggregatedVoteTotals"`
+}
+
+type pollOption struct {
+	ID    string `json:"id"`
+	Label string `json:"label"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	tableName, found := os.LookupEnv(_tableNameEnv)
+	if !found {
+		log.Printf("error environment variable %s not set", _tableNameEnv)
+		return api.InternalServerErrorResponse(), nil
+	}
+
+	db := dynamodb.New(session.Must(session.NewSession()))
+
 	validate, trans, err := validator.NewValidator("en")
 	if err != nil {
-		log.Printf("creating validator: %s", err)
+		log.Printf("error creating validator: %s", err)
 		return api.InternalServerErrorResponse(), nil
 	}
 
 	var createPollReq createPollRequest
 	if err := json.Unmarshal([]byte(request.Body), &createPollReq); err != nil {
-		log.Printf("unmarshalling request body: %s", err)
+		log.Printf("error unmarshalling request body: %s", err)
 		return api.InternalServerErrorResponse(), nil
 	}
 
@@ -44,10 +80,71 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		}, nil
 	}
 
+	newPoll := createPollFromPollRequest(createPollReq)
+
+	if err := storeNewPoll(db, tableName, newPoll); err != nil {
+		return api.InternalServerErrorResponse(), nil
+	}
+
+	res, err := json.Marshal(createPollResponse{ID: newPoll.ID})
+	if err != nil {
+		log.Printf("error marshalling poll response: %s", err)
+		return api.InternalServerErrorResponse(), nil
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       "hi",
+		Body:       string(res),
 		StatusCode: http.StatusAccepted,
 	}, nil
+}
+
+func createPollFromPollRequest(req createPollRequest) poll {
+	id := uuid.NewString()
+	dynamoID := fmt.Sprintf("POLL#%s", id)
+
+	var pollOptions []pollOption
+	for _, o := range req.Options {
+		pollOptions = append(pollOptions, pollOption{
+			ID:    uuid.NewString(),
+			Label: o,
+		})
+	}
+
+	totals := make(map[string]int, len(pollOptions))
+	for _, o := range pollOptions {
+		totals[o.ID] = 0
+	}
+
+	return poll{
+		PK:                   dynamoID,
+		SK:                   dynamoID,
+		ID:                   id,
+		Question:             req.Question,
+		Options:              pollOptions,
+		ChannelARN:           req.ChannelARN,
+		AggregatedVoteTotals: totals,
+	}
+}
+
+func storeNewPoll(db *dynamodb.DynamoDB, tableName string, p poll) error {
+	av, err := dynamodbattribute.MarshalMap(p)
+	if err != nil {
+		log.Printf("error marshalling new poll: %s", err)
+		return err
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = db.PutItem(input)
+	if err != nil {
+		log.Printf("error calling PutItem for poll: %s", err)
+		return err
+	}
+
+	return nil
 }
 
 func main() {
