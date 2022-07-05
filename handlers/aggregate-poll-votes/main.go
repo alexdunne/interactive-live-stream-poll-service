d4606fdea23628baddceef620f961a1336006668
Alex Dunne <hi@alexdunne.net>
2022-07-05 01:26:25 +0100

    implement sending totals to ivs

--- handlers/aggregate-poll-votes/main.go
+++ handlers/aggregate-poll-votes/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,13 +15,25 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/ivs"
 )
 
 const _tableNameEnv = "POLL_TABLE_NAME"
 
+type TotalsPerPoll = map[string]map[string]int
+
 type vote struct {
 	PollID string `json:"pollId"`
 	Answer string `json:"answer"`
+}
+
+type pollResult struct {
+	ID     string         `json:"id"`
+	Totals map[string]int `json:"totals"`
+}
+
+type metadata struct {
+	Data pollResult `json:"data"`
 }
 
 func handle(ctx context.Context, event events.DynamoDBEvent) error {
@@ -29,18 +42,114 @@ func handle(ctx context.Context, event events.DynamoDBEvent) error {
 		return fmt.Errorf("error environment variable %s not set", _tableNameEnv)
 	}
 
-	db := dynamodb.New(session.Must(session.NewSession()))
+	sess := session.Must(session.NewSession())
 
-	totalsPerPoll := make(map[string]map[string]int)
+	db := dynamodb.New(sess)
+	ivsSvc := ivs.New(sess)
 
-	for _, record := range event.Records {
+	totalsPerPoll := aggregatedVoteTotals(event.Records)
+
+	for pollID, totals := range totalsPerPoll {
+		pollDatabaseID := fmt.Sprintf("POLL#%s", pollID)
+
+		type pollUpdate struct {
+			updateExprParts []string
+			attrNames       map[string]*string
+			attrValues      map[string]*dynamodb.AttributeValue
+		}
+
+		update := pollUpdate{}
+		index := 0
+
+		// calculate the updates for the poll
+		for answerID, total := range totals {
+			attrName := fmt.Sprintf("#s_%s", index)
+			attrValKey := fmt.Sprintf(":val_%s", index)
+
+			update.updateExprParts = append(
+				update.updateExprParts,
+				fmt.Sprintf("aggregatedVoteTotal.%s = aggregatedVoteTotal.%s + :val%s", attrName, attrValKey),
+			)
+
+			if _, ok := update.attrNames[attrName]; !ok {
+				update.attrNames[attrName] = aws.String(answerID)
+			}
+			if _, ok := update.attrValues[attrValKey]; !ok {
+				update.attrNames[attrName] = aws.String(strconv.Itoa(total))
+			}
+
+			index = index + 1
+		}
+
+		updateExpr := fmt.Sprintf("set %s", strings.Join(update.updateExprParts, ","))
+
+		input := &dynamodb.UpdateItemInput{
+			TableName: aws.String(tableName),
+			Key: map[string]*dynamodb.AttributeValue{
+				"PK": {
+					S: aws.String(pollDatabaseID),
+				},
+				"SK": {
+					S: aws.String(pollDatabaseID),
+				},
+			},
+			UpdateExpression:          aws.String(updateExpr),
+			ExpressionAttributeNames:  update.attrNames,
+			ExpressionAttributeValues: update.attrValues,
+			ReturnValues:              aws.String("UPDATED_NEW"),
+		}
+
+		res, err := db.UpdateItemWithContext(ctx, input)
+		if err != nil {
+			log.Printf("error updating vote aggregate totals %s", err)
+			continue
+		}
+
+		type updateItemResponse struct {
+			AggregatedVoteTotals map[string]int `json:"aggregatedVoteTotals"`
+		}
+
+		var updateItemRes updateItemResponse
+		if err := dynamodbattribute.UnmarshalMap(res.Attributes, &updateItemRes); err != nil {
+			log.Printf("error unmarshalling update item output attributes %s", err)
+			continue
+		}
+
+		totalsMetadata := metadata{
+			Data: pollResult{
+				ID:     pollID,
+				Totals: updateItemRes.AggregatedVoteTotals,
+			},
+		}
+
+		jsonMetadata, err := json.Marshal(totalsMetadata)
+		if err != nil {
+			log.Printf("error marhsalling totals into metadata: %s", err)
+			continue
+		}
+
+		_, err = ivsSvc.PutMetadataWithContext(ctx, &ivs.PutMetadataInput{
+			ChannelArn: &p.ChannelARN,
+			Metadata:   aws.String(string(jsonMetadata)),
+		})
+		if err != nil {
+			log.Printf("error sending metadata to channel: %s", err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func aggregatedVoteTotals(records []events.DynamoDBEventRecord) TotalsPerPoll {
+	totalsPerPoll := make(TotalsPerPoll)
+
+	for _, record := range records {
 		var v vote
 		if err := unmarshalStreamImage(record.Change.NewImage, &v); err != nil {
 			log.Printf("error unmarshalling stream event into a vote: %s", err)
 			continue
 		}
-
-		log.Printf("received a new vote for poll %s", v.PollID)
 
 		if _, ok := totalsPerPoll[v.PollID]; !ok {
 			totalsPerPoll[v.PollID] = make(map[string]int)
@@ -53,42 +162,7 @@ func handle(ctx context.Context, event events.DynamoDBEvent) error {
 		totalsPerPoll[v.PollID][v.Answer] = totalsPerPoll[v.PollID][v.Answer] + 1
 	}
 
-	for pollID, totals := range totalsPerPoll {
-		pollDatabaseID := fmt.Sprintf("POLL#%s", pollID)
-
-		// being lazy and doing multiple update requests rather than batching
-
-		for answerID, total := range totals {
-			input := &dynamodb.UpdateItemInput{
-				TableName: aws.String(tableName),
-				Key: map[string]*dynamodb.AttributeValue{
-					"PK": {
-						S: aws.String(pollDatabaseID),
-					},
-					"SK": {
-						S: aws.String(pollDatabaseID),
-					},
-				},
-				UpdateExpression: aws.String("set aggregatedVoteTotal.#s = aggregatedVoteTotal.#s + :val"),
-				ExpressionAttributeNames: map[string]*string{
-					"#s": aws.String(answerID),
-				},
-				ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-					":val": {
-						N: aws.String(strconv.Itoa(total)),
-					},
-				},
-			}
-
-			_, err := db.UpdateItemWithContext(ctx, input)
-			if err != nil {
-				log.Printf("error updating vote aggregate totals %s", err)
-				continue
-			}
-		}
-	}
-
-	return nil
+	return totalsPerPoll
 }
 
 func unmarshalStreamImage(attribute map[string]events.DynamoDBAttributeValue, out interface{}) error {
